a831d258ee4f0f42c731d4c402985fa6614a5fe0
Emre Bağdadioğlu <bagdadiogluemre@gmail.com>
2024-03-28 19:15:10 +0300

    initial commit

---
+++ app/src/test/java/com/example/getir_android_kotlin_bootcamp_w2_assignment_emrebagdadioglu/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.example.getir_android_kotlin_bootcamp_w2_assignment_emrebagdadioglu
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
