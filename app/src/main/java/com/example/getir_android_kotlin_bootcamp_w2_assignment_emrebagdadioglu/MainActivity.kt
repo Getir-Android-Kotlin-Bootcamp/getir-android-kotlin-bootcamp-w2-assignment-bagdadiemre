9d52f3cc97919671e308e04136cb3d770149b16b
Emre Bağdadioğlu <bagdadiogluemre@gmail.com>
2024-03-31 12:37:45 +0300

    marker added

--- app/src/main/java/com/example/getir_android_kotlin_bootcamp_w2_assignment_emrebagdadioglu/MainActivity.kt
+++ app/src/main/java/com/example/getir_android_kotlin_bootcamp_w2_assignment_emrebagdadioglu/MainActivity.kt
@@ -1,20 +1,31 @@
 package com.example.getir_android_kotlin_bootcamp_w2_assignment_emrebagdadioglu
 
-import android.content.pm.PackageManager
-import android.location.Location
-import android.os.Bundle
-import android.widget.Toast
 import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.content.res.Resources
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.annotation.DrawableRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.CircleOptions
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MapStyleOptions
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.gms.tasks.OnSuccessListener
 import com.google.android.gms.tasks.Task
@@ -22,6 +33,7 @@ import com.google.android.libraries.places.api.Places
 import com.google.android.libraries.places.api.model.Place
 import com.google.android.libraries.places.widget.AutocompleteSupportFragment
 import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
+
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -37,11 +49,18 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
         //enableEdgeToEdge()
         Places.initialize(applicationContext, getString(R.string.google_maps_key))
-        autoCompleteFragment = supportFragmentManager.findFragmentById(R.id.autocomplete_fragment) as AutocompleteSupportFragment
-        val info : CardView = findViewById(R.id.infoCardView)
+        autoCompleteFragment =
+            supportFragmentManager.findFragmentById(R.id.autocomplete_fragment) as AutocompleteSupportFragment
+        val info: CardView = findViewById(R.id.infoCardView)
         autoCompleteFragment.setHint("Where is your location ?")
-        autoCompleteFragment.setPlaceFields(listOf(Place.Field.ID, Place.Field.ADDRESS, Place.Field.LAT_LNG))
-        autoCompleteFragment.setOnPlaceSelectedListener(object: PlaceSelectionListener {
+        autoCompleteFragment.setPlaceFields(
+            listOf(
+                Place.Field.ID,
+                Place.Field.ADDRESS,
+                Place.Field.LAT_LNG
+            )
+        )
+        autoCompleteFragment.setOnPlaceSelectedListener(object : PlaceSelectionListener {
             override fun onPlaceSelected(place: Place) {
                 val latLng = place.latLng
                 if (latLng != null) {
@@ -50,15 +69,17 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
             }
 
             override fun onError(status: com.google.android.gms.common.api.Status) {
-                Toast.makeText(this@MainActivity, "An error occurred: $status", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@MainActivity, "An error occurred: $status", Toast.LENGTH_SHORT)
+                    .show()
             }
         })
 
-        val mapFragment = supportFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment
+        val mapFragment =
+            supportFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
-    private fun zoomOnMap(latLng: LatLng){
+    private fun zoomOnMap(latLng: LatLng) {
         val newLatLngZoom = CameraUpdateFactory.newLatLngZoom(latLng, 12f)
         mGoogleMap?.moveCamera(newLatLngZoom)
     }
@@ -67,17 +88,45 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         mGoogleMap = googleMap
 
         // Check for location permission
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-            && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
         ) {
             // Request location permissions if not granted
             ActivityCompat.requestPermissions(
                 this,
-                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION),
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ),
                 PERMISSIONS_REQUEST_LOCATION
             )
             return
         }
+
+        // Set Marker
+
+
+        try {
+
+            val success = googleMap.setMapStyle(
+                MapStyleOptions.loadRawResourceStyle(
+                    this, R.raw.map
+                )
+            )
+
+            if (!success) {
+                Log.e("MapsActivity", "Style parsing failed.")
+            }
+        } catch (e: Resources.NotFoundException) {
+            Log.e("MapsActivity", "Can't find style. Error: ", e)
+        }
+
 
         // Get current location
         val locationTask: Task<android.location.Location> = fusedLocationClient.lastLocation
@@ -90,13 +139,55 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
                 val markerOptions = MarkerOptions()
                     .position(currentLatLng)
                     .title("Current Location")
-                mGoogleMap?.addMarker(markerOptions)
+                    .icon(bitmapDescriptorFromVector(this@MainActivity, R.drawable.ic_marker))
+                    .anchor(0.5f, 0.5f)
+
+                val marker = mGoogleMap?.addMarker(markerOptions)
+
+// Add a circle around the marker
+                val circle = mGoogleMap?.addCircle(
+                    CircleOptions()
+                        .center(currentLatLng)
+                        .radius(130.0) // radius in meters
+                        .strokeColor(Color.TRANSPARENT) // "#f6dee0"
+                        .fillColor(Color.parseColor("#80f5b0b7")) // fill color, transparent in this case
+                )
+
+// Associate the circle with the marker for management purposes
+                marker?.tag = circle
             } else {
-                Toast.makeText(this@MainActivity, "Current location is not available", Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    this@MainActivity,
+                    "Current location is not available",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
         })
     }
+
     companion object {
         private const val PERMISSIONS_REQUEST_LOCATION = 100
     }
+
+    private fun bitmapDescriptorFromVector(
+        context: Context,
+        @DrawableRes vectorResId: Int
+    ): BitmapDescriptor {
+        val vectorDrawable = ContextCompat.getDrawable(context, vectorResId)
+        vectorDrawable!!.setBounds(
+            0,
+            0,
+            vectorDrawable!!.intrinsicWidth,
+            vectorDrawable!!.intrinsicHeight
+        )
+        val bitmap = Bitmap.createBitmap(
+            vectorDrawable!!.intrinsicWidth,
+            vectorDrawable!!.intrinsicHeight,
+            Bitmap.Config.ARGB_8888
+        )
+        val canvas = Canvas(bitmap)
+        vectorDrawable!!.draw(canvas)
+        return BitmapDescriptorFactory.fromBitmap(bitmap)
+    }
+
 }
